707300ad4bacffce9bd86ae45ecf3a1d5ffe5303
Stev Witzel <stev.witzel@gmail.com>
2015-10-01 21:28:31 +0100

    Add main functionality to Eureka client

--- discovery/eureka/eureka.go
+++ discovery/eureka/eureka.go
@@ -1,9 +1,13 @@
 package eureka
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/hudl/fargo"
+	"github.com/st3v/cfkit/env"
 )
 
 const (
@@ -11,18 +15,49 @@ const (
 	appStdSecurePort = 443
 )
 
-func NewClient(uris []string, port int, timeout, pollInterval time.Duration) *fargoClient {
-	return &fargoClient{fargoConn(uris, port, timeout, pollInterval)}
+var connProvider = fargoConn
+
+func NewClient(uris []string, port int, timeout, pollInterval time.Duration) *client {
+	return &client{
+		conn:         connProvider(uris, port, timeout, pollInterval),
+		uris:         uris,
+		port:         port,
+		timeout:      timeout,
+		pollInterval: pollInterval,
+	}
 }
 
 type Client interface {
+	Register(app env.App) error
+	Deregister(app env.App) error
+	Heartbeat(app env.App) error
+	Apps() ([]Application, error)
+	App(name string) (Application, error)
 	URIs() []string
 	Port() int
 	Timeout() time.Duration
 	PollInterval() time.Duration
 }
 
-func fargoConn(uris []string, port int, timeout, pollInterval time.Duration) *fargo.EurekaConnection {
+type Application struct {
+	Name      string
+	Instances []Instance
+}
+
+type Instance struct {
+	ID  string
+	URI string
+}
+
+type FargoConnection interface {
+	RegisterInstance(*fargo.Instance) error
+	DeregisterInstance(*fargo.Instance) error
+	HeartBeatInstance(*fargo.Instance) error
+	GetApp(string) (*fargo.Application, error)
+	GetApps() (map[string]*fargo.Application, error)
+}
+
+func fargoConn(uris []string, port int, timeout, pollInterval time.Duration) FargoConnection {
 	return &fargo.EurekaConnection{
 		ServiceUrls:  uris,
 		ServicePort:  port,
@@ -32,34 +67,114 @@ func fargoConn(uris []string, port int, timeout, pollInterval time.Duration) *fa
 	}
 }
 
-type fargoClient struct {
-	conn *fargo.EurekaConnection
+type client struct {
+	conn         FargoConnection
+	uris         []string
+	port         int
+	timeout      time.Duration
+	pollInterval time.Duration
 }
 
-func (c *fargoClient) URIs() []string {
-	if c.conn == nil {
-		return []string{}
+func fargoInstance(app env.App) *fargo.Instance {
+	return &fargo.Instance{
+		HostName:         app.URI(),
+		PortJ:            fargo.Port{strconv.Itoa(appStdPort), "true"},
+		SecurePortJ:      fargo.Port{strconv.Itoa(appStdSecurePort), "true"},
+		App:              strings.ToUpper(app.Name),
+		IPAddr:           app.Instance.Addr,
+		VipAddress:       app.URI(),
+		Status:           fargo.UP,
+		Overriddenstatus: fargo.UNKNOWN,
+		DataCenterInfo:   fargo.DataCenterInfo{Name: fargo.MyOwn},
+		Metadata:         fargo.InstanceMetadata{Raw: []byte(fmt.Sprintf(`{"instanceId": "%s"}`, app.Instance.ID))},
+		UniqueID: func(i fargo.Instance) string {
+			return fmt.Sprintf("%s:%s", app.URI(), app.Instance.ID)
+		},
 	}
-	return c.conn.ServiceUrls
 }
 
-func (c *fargoClient) Port() int {
-	if c.conn == nil {
-		return 0
+func (c *client) Register(app env.App) error {
+	err := c.conn.RegisterInstance(fargoInstance(app))
+	if err != nil {
+		return fmt.Errorf("Error registering app with Eureka: %s", err)
 	}
-	return c.conn.ServicePort
+	return nil
 }
 
-func (c *fargoClient) Timeout() time.Duration {
-	if c.conn == nil {
-		return 0
+func (c *client) Deregister(app env.App) error {
+	err := c.conn.DeregisterInstance(fargoInstance(app))
+	if err != nil {
+		return fmt.Errorf("Error deregistering app with Eureka: %s", err)
 	}
-	return c.conn.Timeout
+	return nil
 }
 
-func (c *fargoClient) PollInterval() time.Duration {
-	if c.conn == nil {
-		return 0
+func (c *client) Heartbeat(app env.App) error {
+	err := c.conn.HeartBeatInstance(fargoInstance(app))
+	if err != nil {
+		return fmt.Errorf("Error sending heartbeat for app to Eureka: %s", err)
 	}
-	return c.conn.PollInterval
+	return nil
+}
+
+func (c *client) Apps() ([]Application, error) {
+	apps, err := c.conn.GetApps()
+	if err != nil {
+		return []Application{}, fmt.Errorf("Error retrieving apps from Eureka: %s", err)
+	}
+
+	result := []Application{}
+	for _, app := range apps {
+		instances := make([]Instance, len(app.Instances))
+
+		for i, inst := range app.Instances {
+			instances[i] = Instance{
+				ID:  inst.Id(),
+				URI: inst.VipAddress,
+			}
+		}
+
+		result = append(result, Application{
+			Name:      app.Name,
+			Instances: instances,
+		})
+	}
+
+	return result, nil
+}
+
+func (c *client) App(name string) (Application, error) {
+	app, err := c.conn.GetApp(name)
+	if err != nil {
+		return Application{}, fmt.Errorf("Error retrieving app '%s' from Eureka: %s", name, err)
+	}
+
+	instances := make([]Instance, len(app.Instances))
+	for i, inst := range app.Instances {
+		instances[i] = Instance{
+			ID:  inst.Id(),
+			URI: inst.VipAddress,
+		}
+	}
+
+	return Application{
+		Name:      app.Name,
+		Instances: instances,
+	}, nil
+}
+
+func (c *client) URIs() []string {
+	return c.uris
+}
+
+func (c *client) Port() int {
+	return c.port
+}
+
+func (c *client) Timeout() time.Duration {
+	return c.timeout
+}
+
+func (c *client) PollInterval() time.Duration {
+	return c.pollInterval
 }
--- discovery/eureka/eureka_test.go
+++ discovery/eureka/eureka_test.go
@@ -1,11 +1,16 @@
 package eureka
 
 import (
+	"errors"
+	"fmt"
+	"strings"
 	"time"
 
 	"github.com/hudl/fargo"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/st3v/cfkit/discovery/eureka/fake"
+	"github.com/st3v/cfkit/env"
 )
 
 var _ = Describe("eureka", func() {
@@ -19,90 +24,237 @@ var _ = Describe("eureka", func() {
 	Describe(".NewClient", func() {
 		It("returns a client with a correctly initialized fargo connection", func() {
 			c := NewClient(expectedURIs, expectedPort, expectedTimeout, expectedPollInterval)
-			Expect(c.conn.ServiceUrls).To(HaveLen(len(expectedURIs)))
-			Expect(c.conn.ServiceUrls).To(ConsistOf(expectedURIs))
-			Expect(c.conn.ServicePort).To(Equal(expectedPort))
-			Expect(c.conn.Timeout).To(Equal(expectedTimeout))
-			Expect(c.conn.PollInterval).To(Equal(expectedPollInterval))
+
+			conn, ok := c.conn.(*fargo.EurekaConnection)
+			Expect(ok).To(BeTrue())
+
+			Expect(conn.ServiceUrls).To(HaveLen(len(expectedURIs)))
+			Expect(conn.ServiceUrls).To(ConsistOf(expectedURIs))
+			Expect(conn.ServicePort).To(Equal(expectedPort))
+			Expect(conn.Timeout).To(Equal(expectedTimeout))
+			Expect(conn.PollInterval).To(Equal(expectedPollInterval))
 		})
 	})
 
-	Describe("fargoClient", func() {
-		var client *fargoClient
+	Describe("client", func() {
+		var (
+			client *client
 
-		BeforeEach(func() {
-			client = &fargoClient{
-				conn: &fargo.EurekaConnection{
-					ServiceUrls:  expectedURIs,
-					ServicePort:  expectedPort,
-					Timeout:      expectedTimeout,
-					PollInterval: expectedPollInterval,
+			fakeConn         *fake.FargoConnection
+			origConnProvider = fargoConn
+
+			fargoApp  *fargo.Application
+			fargoApps map[string]*fargo.Application
+
+			app = env.App{
+				ID:   "app-id",
+				Name: "app-name",
+				URIs: []string{"app-uri-1", "app-uri-2"},
+				Host: "app-host",
+				Port: 12345,
+				Addr: "app-addr",
+				Instance: env.AppInstance{
+					ID:    "app-instance_id",
+					Index: 99,
 				},
 			}
+
+			assertInstance = func(i *fargo.Instance) {
+				Expect(i.HostName).To(Equal(app.URI()))
+				Expect(i.PortJ.Number).To(Equal("80"))
+				Expect(i.PortJ.Enabled).To(Equal("true"))
+				Expect(i.SecurePortJ.Number).To(Equal("443"))
+				Expect(i.SecurePortJ.Enabled).To(Equal("true"))
+				Expect(i.App).To(Equal(strings.ToUpper(app.Name)))
+				Expect(i.IPAddr).To(Equal(app.Instance.Addr))
+				Expect(i.VipAddress).To(Equal(app.URI()))
+				Expect(i.Status).To(Equal(fargo.UP))
+				Expect(i.Overriddenstatus).To(Equal(fargo.UNKNOWN))
+				Expect(i.DataCenterInfo.Name).To(Equal(fargo.MyOwn))
+				Expect(i.UniqueID).ToNot(BeNil())
+				Expect(i.UniqueID(*i)).To(Equal(fmt.Sprintf("%s:%s", app.URI(), app.Instance.ID)))
+			}
+		)
+
+		BeforeEach(func() {
+			fakeConn = new(fake.FargoConnection)
+
+			fargoApp = &fargo.Application{
+				Name: app.Name,
+				Instances: []*fargo.Instance{{
+					UniqueID: func(fargo.Instance) string {
+						return app.Instance.ID
+					},
+					HostName: app.URI(),
+				}},
+			}
+			fakeConn.GetAppReturns(fargoApp, nil)
+
+			fargoApps = map[string]*fargo.Application{
+				fargoApp.Name: fargoApp,
+			}
+			fakeConn.GetAppsReturns(fargoApps, nil)
+
+			connProvider = func(uris []string, port int, timeout, pollInterval time.Duration) FargoConnection {
+				return fakeConn
+			}
+
+			client = NewClient(expectedURIs, expectedPort, expectedTimeout, expectedPollInterval)
+		})
+
+		AfterEach(func() {
+			connProvider = origConnProvider
+		})
+
+		Describe(".Register", func() {
+			It("calls conn.RegisterInstance with the correct instance", func() {
+				client.Register(app)
+				Expect(fakeConn.RegisterInstanceCallCount()).To(Equal(1))
+				assertInstance(fakeConn.RegisterInstanceArgsForCall(0))
+			})
+
+			Context("when conn.RegisterInstance returns an error", func() {
+				var expectedErr = errors.New("some-error")
+
+				BeforeEach(func() {
+					fakeConn.RegisterInstanceReturns(expectedErr)
+				})
+
+				It("returns the error", func() {
+					err := client.Register(app)
+					Expect(err).To(HaveOccurred())
+					Expect(err.Error()).To(ContainSubstring("Error registering app"))
+					Expect(err.Error()).To(ContainSubstring(expectedErr.Error()))
+				})
+			})
+		})
+
+		Describe(".Deregister", func() {
+			It("calls conn.DeregisterInstance with the correct instance", func() {
+				client.Deregister(app)
+				Expect(fakeConn.DeregisterInstanceCallCount()).To(Equal(1))
+				assertInstance(fakeConn.DeregisterInstanceArgsForCall(0))
+			})
+
+			Context("when conn.RegisterInstance returns an error", func() {
+				var expectedErr = errors.New("some-error")
+
+				BeforeEach(func() {
+					fakeConn.DeregisterInstanceReturns(expectedErr)
+				})
+
+				It("returns the error", func() {
+					err := client.Deregister(app)
+					Expect(err).To(HaveOccurred())
+					Expect(err.Error()).To(ContainSubstring("Error deregistering app"))
+					Expect(err.Error()).To(ContainSubstring(expectedErr.Error()))
+				})
+			})
+		})
+
+		Describe(".Heartbeat", func() {
+			It("calls conn.HeartBeatInstance with the correct instance", func() {
+				client.Heartbeat(app)
+				Expect(fakeConn.HeartBeatInstanceCallCount()).To(Equal(1))
+				assertInstance(fakeConn.HeartBeatInstanceArgsForCall(0))
+			})
+
+			Context("when conn.HeartBeatInstance returns an error", func() {
+				var expectedErr = errors.New("some-error")
+
+				BeforeEach(func() {
+					fakeConn.HeartBeatInstanceReturns(expectedErr)
+				})
+
+				It("returns the error", func() {
+					err := client.Heartbeat(app)
+					Expect(err).To(HaveOccurred())
+					Expect(err.Error()).To(ContainSubstring("Error sending heartbeat for app"))
+					Expect(err.Error()).To(ContainSubstring(expectedErr.Error()))
+				})
+			})
+		})
+
+		Describe(".App", func() {
+			It("calls conn.GetApp with the correct app name", func() {
+				client.App("foo")
+				Expect(fakeConn.GetAppCallCount()).To(Equal(1))
+				Expect(fakeConn.GetAppArgsForCall(0)).To(Equal("foo"))
+			})
+
+			It("returns the app retrieved from conn.GetApp", func() {
+				app, _ := client.App("foo")
+				Expect(app.Name).To(Equal(fargoApp.Name))
+			})
+
+			Context("when conn.GetApp returns an error", func() {
+				var expectedErr = errors.New("some-error")
+
+				BeforeEach(func() {
+					fakeConn.GetAppReturns(nil, expectedErr)
+				})
+
+				It("returns the error", func() {
+					_, err := client.App("foo")
+					Expect(err).To(HaveOccurred())
+					Expect(err.Error()).To(ContainSubstring("Error retrieving app 'foo'"))
+					Expect(err.Error()).To(ContainSubstring(expectedErr.Error()))
+				})
+			})
+		})
+
+		Describe(".Apps", func() {
+			It("calls conn.GetApps", func() {
+				client.Apps()
+				Expect(fakeConn.GetAppsCallCount()).To(Equal(1))
+			})
+
+			It("returns the apps retrieved from conn.GetApps", func() {
+				apps, _ := client.Apps()
+				Expect(apps).To(HaveLen(len(fargoApps)))
+				for _, app := range apps {
+					Expect(app.Name).To(Equal(fargoApp.Name))
+				}
+			})
+
+			Context("when conn.GetApps returns an error", func() {
+				var expectedErr = errors.New("some-error")
+
+				BeforeEach(func() {
+					fakeConn.GetAppsReturns(nil, expectedErr)
+				})
+
+				It("returns the error", func() {
+					_, err := client.Apps()
+					Expect(err).To(HaveOccurred())
+					Expect(err.Error()).To(ContainSubstring("Error retrieving apps"))
+					Expect(err.Error()).To(ContainSubstring(expectedErr.Error()))
+				})
+			})
 		})
 
 		Describe(".URIs", func() {
-			It("returns the service URIs of the underlying connection", func() {
+			It("returns the correct URIs", func() {
 				Expect(client.URIs()).To(HaveLen(len(expectedURIs)))
 				Expect(client.URIs()).To(ConsistOf(expectedURIs))
-			})
-
-			Context("when the underlying connection is nil", func() {
-				BeforeEach(func() {
-					client = new(fargoClient)
-				})
-
-				It("returns an empty slice", func() {
-					Expect(client.URIs()).To(HaveLen(0))
-				})
 			})
 		})
 
 		Describe(".Port", func() {
-			It("returns the service port of the underlying connection", func() {
+			It("returns the correct port", func() {
 				Expect(client.Port()).To(Equal(expectedPort))
-			})
-
-			Context("when the underlying connection is nil", func() {
-				BeforeEach(func() {
-					client = new(fargoClient)
-				})
-
-				It("returns zero", func() {
-					Expect(client.Port()).To(BeZero())
-				})
 			})
 		})
 
 		Describe(".Timeout", func() {
-			It("returns the timeout for the underlying connection", func() {
+			It("returns the correct timeout", func() {
 				Expect(client.Timeout()).To(Equal(expectedTimeout))
-			})
-
-			Context("when the underlying connection is nil", func() {
-				BeforeEach(func() {
-					client = new(fargoClient)
-				})
-
-				It("returns zero", func() {
-					Expect(client.Timeout()).To(BeZero())
-				})
 			})
 		})
 
 		Describe(".PollInterval", func() {
-			It("returns the poll intervall for the underlying connection", func() {
+			It("returns the correct poll intervall", func() {
 				Expect(client.PollInterval()).To(Equal(expectedPollInterval))
-			})
-
-			Context("when the underlying connection is nil", func() {
-				BeforeEach(func() {
-					client = new(fargoClient)
-				})
-
-				It("returns zero", func() {
-					Expect(client.PollInterval()).To(BeZero())
-				})
 			})
 		})
 	})
